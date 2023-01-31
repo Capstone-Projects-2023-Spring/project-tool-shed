b194564af45bdddf17788942ca904109e0560b72
Nate Symer <nate@symer.io>
2023-01-31 11:07:14 -0500

    Accommodate more kinds of docker installs for running postgres locally

--- scripts/run-postgres.sh
+++ scripts/run-postgres.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env sh
 
-mkdir .pgdata
+PASSWORD=password
 
-sudo docker run \
+mkdir .pgdata 2>/dev/null > /dev/null
+
+## A simple proc for determining if the current
+## user is in a group.
+ingroup(){ [[ " `id -Gn $2` " == *" $1 "* ]]; }
+
+# Determine if we need to use sudo
+PFX='sudo'
+
+if ingroup docker; then
+	PFX=''
+elif [[ $EUID = 0 ]]; then  
+	PFX=''
+fi
+
+$PFX docker run \
   -v $(pwd)/.pgdata:/var/lib/postgresql/data \
-  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_PASSWORD=$PASSWORD \
   -p 5432:5432 \
   postgres

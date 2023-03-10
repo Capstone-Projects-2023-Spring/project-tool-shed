7ecf03e0e4f3e778c9de2d2796b31f25e58a0781
Nate Symer <nate@symer.io>
2023-03-10 14:03:14 -0500

    Write FTS

--- validators/loginUser.js
+++ validators/loginUser.js
@@ -1,8 +1,8 @@
 const yup = require('yup');
 
 const loginUserSchema = yup.object({
-	email: yup.string().email(),
-	password: yup.string()
+	email: yup.string().email().required(),
+	password: yup.string().required()
 });
 
 module.exports = loginUserSchema;

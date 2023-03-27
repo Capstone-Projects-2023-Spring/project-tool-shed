f7615cc437905295680abb80fc4ec14a4b432116
zktejaka <tejadaki@gmail.com>
2023-03-26 22:08:23 -0400

    Big scary warning

--- public/reviews.jsx
+++ public/reviews.jsx
@@ -6,6 +6,16 @@ const Review = (function () {
         boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)"
     };
 
+    const warningStyle = {
+        color: "red",
+        fontWeight: "bold",
+        fontSize: "1.2rem",
+        padding: "10px",
+        backgroundColor: "#FFD1D1",
+        border: "2px solid #FF4F4F",
+        borderRadius: "4px"
+    };
+
     return ({ 
         email,
         reviewer_id,
@@ -18,10 +28,21 @@ const Review = (function () {
             return "TODO";
         }
 
-        return <div style={style}>
-            <h2>Reviewer: {email} {reviewer_id}</h2>
-            <p>{content}</p>
-            <p>ratings: {ratings}/5</p>
-        </div>
+        let warning1 = '';
+        let warning2 = '';
+        if (ratings == 0) {
+            warning1 = <p style={warningStyle}>&#128295; &#128295; WARNING &#128295; &#128295; </p>
+            warning2 = <p style={warningStyle}> You have received a 0/5 rating and now have an infraction on your account </p>
+        }
+
+        return (
+            <div style={style}>
+                <h2>Reviewer: {email} {reviewer_id}</h2>
+                <p>{content}</p>
+                <p>ratings: {ratings}/5</p>
+                {warning1}
+                {warning2}
+            </div>
+        );
     };
 })();

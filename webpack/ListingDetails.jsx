dcee6abaa28adca3a1e2e74a0032d6e62fbb2e09
zktejaka <tejadaki@gmail.com>
2023-04-07 08:55:49 -0400

    Beautify Listing Details Page

--- webpack/ListingDetails.jsx
+++ webpack/ListingDetails.jsx
@@ -1,31 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-/**
- * A Listing object on this page represents a listing a user is interested in seeing details for
- * 
- */
-
 const style = {
-  backgroundColor: "#F1F1F1",
-  padding: 5,
-  borderRadius: 4,
-  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#F1F1F1",
+    padding: 10,
+    borderRadius: 4,
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+    width: "50%",
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 36,
+  },
   button: {
-    background: "#1e90ff",
-    color: "#fff",
-    fontSize: "1.2em",
-    padding: "10px 20px",
-    border: "none",
+    fontWeight: "bold",
+    color: "white",
+    backgroundColor: "#0077ff",
+    padding: "10px 15px",
     borderRadius: "4px",
-    textDecoration: "none",
-    cursor: "pointer"
-  }
+    border: "none",
+    cursor: "pointer",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginBottom: 5,
+  },
+  info: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  videoContainer: {
+    marginTop: 20,
+  },
 };
-
-/*
-     * Creates a new Listing component with the given information
-     */
 
 const Listing = ({
   owner,
@@ -34,45 +52,48 @@ const Listing = ({
   maxBillingIntervals,
   tool,
 }) => {
-	const s = tool.name + " " + tool.description;
-    if(tool.video == 'https://www.youtube.com/'){
-        tool.video = `https://www.youtube.com/results?search_query=${encodeURIComponent(s)}`
-    }
-    const realLink = tool.video;
-    const videoId = getVideoId(realLink);
+  const s = tool.name + " " + tool.description;
+  if (tool.video === 'https://www.youtube.com/') {
+    tool.video = `https://www.youtube.com/results?search_query=${encodeURIComponent(s)}`;
+  }
+  const realLink = tool.video;
+  const videoId = getVideoId(realLink);
 
-    return (
-      <div style={style}>
-        <h2 style={{ fontWeight: "bold" }}>
-          <p>
-            {tool.name} {owner}
-          </p>
+  return (
+    <div style={style.container}>
+      <div style={style.header}>
+        <h2 style={style.title}>
+          {tool.name}
         </h2>
-        <p>Tool Description: {tool.description}</p>
-        <p>Price & Rate : ${price} {billingInterval}</p>
-        <p>Available For: {maxBillingIntervals} units</p>
-        <div>
-          {videoId ? (
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${videoId}`}
-              allow="autoplay; encrypted-media"
-              allowFullScreen
-            />
-          ) : (
-            <a href={realLink}target="_blank" rel="noopener noreferrer">
-            <button style={style.button}>Recommended Videos</button>
-          </a>
-          
-          )}
-        </div>
         <a href={`/inbox/${tool.owner_id}`}>
-          <button style={{fontWeight: "bold", color: "white", backgroundColor: "#0077ff", padding: "10px 15px", borderRadius: "4px", border: "none", cursor: "pointer"}}>Contact Owner</button>
+          <button style={style.button}>Contact Owner</button>
         </a>
       </div>
-    );
-  };
+      <div>
+        <p style={style.subtitle}>
+          {tool.description}
+        </p>
+        <p style={style.info}>
+          Price & Rate: ${price} {billingInterval}
+        </p>
+        <p style={style.info}>
+          Available For: {maxBillingIntervals} units
+        </p>
+      </div>
+      {videoId && (
+        <div style={style.videoContainer}>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 function getVideoId(videoUrl) {
   const regex = /(?:\/|v=)([\w-]{11})(?:\?|&|$)/;
@@ -86,6 +107,5 @@ const ListingDetails = ({ listings }) => {
   </div>;
 };
 
-// use ReactDOM.createRoot to render the component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<ListingDetails listings={window._listings} />);

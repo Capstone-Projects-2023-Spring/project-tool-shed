41780693081c10ecb81be395236e59005ac8eb99
zktejaka <tejadaki@gmail.com>
2023-04-25 17:12:57 -0400

    Updated UI on Listing Details page

--- webpack/components/Listing.jsx
+++ webpack/components/Listing.jsx
@@ -32,19 +32,19 @@ const Listing = ({ id: listingId, price, billingInterval, maxBillingIntervals, t
 					{!isOwn && <LinkButton url={`/inbox/${tool.owner.id}/?listingId=${listingId}`}>Contact {tool.owner.first_name}</LinkButton>}
 					{isOwn && <LinkButton url={`/tools/${tool.id}/edit`}>Edit</LinkButton>}
 				</Flex>
-				<Image src={photoURL} alt={photoName}/>
+
 			</CardHeader>
 			<CardBody>
-				<Text>{tool.description}</Text>
-
+				<Image src={photoURL} alt={photoName} style={{ width: 640, height: 480 }}/>
+				<Text fontSize="2xl">{tool.description}</Text>
 				{isPrimary && videoId && <Video url={`https://www.youtube.com/embed/${videoId}`} />}
 				{isPrimary && !videoId && tool.video && <Video url={tool.video} />}
 				{isPrimary && !videoId && !tool.video && <Link href={getSearchURL(tool)} isExternal>Videos on youtube <ExternalLinkIcon /></Link>}
 			</CardBody>
 			<CardFooter>
 				{isRental ?
-				<Text>For rent for ${price} {billingInterval} (max {maxBillingIntervals} {biNoun})</Text>
-				: <Text>For sale for ${price}</Text>}
+				<Text fontSize="2xl">For rent for ${price} {billingInterval} (max {maxBillingIntervals} {biNoun})</Text>
+				: <Text fontSize="2xl"> For sale for ${price}</Text>}
 			</CardFooter>
 		</Card>
 	);
